c1a88824743d09f3ab912cf3d33e9f008c5d8256
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2019-06-15 12:03:20 -0300

    Renamed test.fxml to easternui.fxml

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,10 @@
 
 import com.easternmind.savemanipulator.*;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class Main extends Application {
     //Eastern Mind Save Editor Class
@@ -17,18 +12,13 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("com/easternmind/savemanipulator/test.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("com/easternmind/savemanipulator/easternui.fxml"));
         primaryStage.setTitle("Eastern Mind Save Editor");
         primaryStage.setScene(new Scene(root, 390, 275));
         primaryStage.setResizable(false);
         primaryStage.show();
 
         Scene scene = primaryStage.getScene();
-        //System.out.println(choiceBox.getValue());
-    }
-
-    public void CreateCheckboxItems(){
-
     }
 
     public static void main(String[] args) {
